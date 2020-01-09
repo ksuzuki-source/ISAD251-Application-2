c4fa271a74bb97843f83643a01862b415fa78789
ksuzuki-source <56300054+ksuzuki-source@users.noreply.github.com>
2020-01-09 20:17:32 +0000

    SQLQuery

---
+++ SQL/SQLQuery.sql
@@ -0,0 +1,22 @@
+﻿
+CREATE TABLE [dbo].[Customers] (
+    [TableId] INT IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([TableId] ASC)
+);
+
+CREATE TABLE [dbo].[OrderDetails] (
+    [OrderId]   INT      IDENTITY (1, 1) NOT NULL,
+    [ProductId] INT      NOT NULL,
+    [Quantity]  INT      NOT NULL,
+    [Date]      DATETIME NOT NULL,
+    CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED ([OrderId] ASC)
+);
+
+
+CREATE TABLE [dbo].[Products] (
+    [ProductId]       INT           IDENTITY (1, 1) NOT NULL,
+    [Product_Details] VARCHAR (MAX) NOT NULL,
+    [Price]           FLOAT (53)    NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
